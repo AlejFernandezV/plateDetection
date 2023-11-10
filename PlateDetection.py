d1d67fc9839bddfd0ddb875343e29eb0699ae6c4
AlejFernandezV <alejfernandez@unicauca.edu.co>
2023-11-10 07:36:15 -0500

    Adding the train file and some Data

--- PlateDetection.py
+++ PlateDetection.py
@@ -1,22 +1,36 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('BestTrain/weights/best.pt')
+modelPlates = YOLO('BestTrain/weights/plates/best.pt')
+modelLN = YOLO('BestTrain/weights/letters_numbers/bestFull30.pt')
 
 cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('Videos/v3.mp4')
+ctexto = ""
+# cap = cv2.VideoCapture('Videos/v3.mp4')
 
 while cap.isOpened():
 
     success, frame = cap.read()
     if success:
 
-        results = model(frame, imgsz=640)
-        annotated_frame = results[0].plot()
+        resultsPlates = modelPlates(frame, imgsz=640)
+        
+        for result in resultsPlates.xyxy[0]:
+            x, y, w, h = map(int, result[:4])
+            roi = frame[y:h, x:w]
 
-        cv2.imshow("Plates", annotated_frame)
+            resultsLN = modelLN(roi, imgsz=640)
+            letras_numeros = resultsLN[0].plot()
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.imshow("Letters and Numbers", letras_numeros)
+
+        placas = resultsPlates[0].plot()
+
+        cv2.imshow("Plates", placas)
+
+        tecla = cv2.waitKey(1)
+
+        if tecla == 27:
             break
     else:
         break
--- dataset roboflow/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Proyecto Placas > Primera version
-https://universe.roboflow.com/juan-felipe-orozco-cortes/proyecto-placas
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
